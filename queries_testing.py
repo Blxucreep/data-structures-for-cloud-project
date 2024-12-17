2a9af22f3aa4f18d8bdf769ef424871c96219d2a
Blxucreep <LoevanLqc@outlook.com>
2024-12-17 19:26:58 +0100

    final scripts

--- queries_testing.py
+++ queries_testing.py
@@ -1,24 +1,12 @@
-from sshtunnel import SSHTunnelForwarder
 import pymongo
 import json
 import time
 import statistics
 
-MONGO_HOST = "mesiin59202401009.westeurope.cloudapp.azure.com"
-MONGO_DB = "epic_games_store"
-MONGO_USER = "administrateur"
-MONGO_PASS = "SuperPassword!1"
+MONGO_HOST = "40.118.102.86"
+MONGO_DB = "epic"
 
-server = SSHTunnelForwarder(
-    MONGO_HOST,
-    ssh_username=MONGO_USER,
-    ssh_password=MONGO_PASS,
-    remote_bind_address=('127.0.0.1', 27017)
-)
-
-server.start()
-client = pymongo.MongoClient('127.0.0.1', server.local_bind_port) # server.local_bind_port
-                                                                  # is assigned local port
+client = pymongo.MongoClient(MONGO_HOST, 30000)
 db = client[MONGO_DB]
 
 # function to load queries from a file
@@ -27,32 +15,27 @@ def load_queries(file_path):
         return json.load(f)
 
 # function to execute a query
-def test_query(query_name):
+def test_query(query_name, collection_name):
     queries = load_queries('projet/queries_to_test.json')
     query_info = queries.get(query_name)
     if query_info is None:
         raise ValueError(f"No query found with name '{query_name}'")
 
-    collection = db[query_info['collection']]
-    if query_info['function'] == 'find':
-        result = list(collection.find(query_info['filter']))
-    elif query_info['function'] == 'aggregate':
-        result = list(collection.aggregate(query_info['pipeline']))
-    else:
-        raise ValueError(f"Unsupported function '{query_info['function']}'")
+    collection = db[collection_name]
+    start_time = time.time()
+    result = list(collection.aggregate(query_info['pipeline']))
+    process_time = time.time() - start_time
 
-    return result
+    return result, process_time
 
 # measure performance
-def measure_performance(query_name, iterations=10):
+def measure_performance(query_name, collection_name, iterations=10):
     execution_times = []
     all_results = None # to store the results from one execution (they should be the same)
     start_time = None
 
     for _ in range(iterations):
-        start_time = time.time()
-        result = test_query(query_name)
-        execution_time = time.time() - start_time
+        result, execution_time = test_query(query_name, collection_name)
         execution_times.append(execution_time)
         
         # store the results only once (from the first iteration)
@@ -68,12 +51,11 @@ def measure_performance(query_name, iterations=10):
     return avg_time, execution_times, all_results
 
 # execute the query
-query_name = "query_5"
-avg_time, times, results = measure_performance(query_name)
+query_name = "query_1"
+collection_name = "games_user_view"
+avg_time, times, results = measure_performance(query_name, collection_name)
 
 # print results
 print(f"Query '{query_name}'")
-print(f"Mean execution time: {avg_time:.4f} seconds")
+print(f"Mean execution time: {avg_time:.6f} seconds")
 print(f"Number of documents returned: {len(results)}")
-
-server.stop()
